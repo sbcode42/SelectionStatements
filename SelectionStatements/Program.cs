3824efe0f54e968650e04a0b215a6a22afc1d2d7
Sami Booth <ballersabooth@yahoo.com>
2020-08-25 14:07:32 -0500

    excercise one done

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low!");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine($"Too High!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine("What is your favorite school subject?");
+            var userSubject = (Console.ReadLine());
+           
+            switch(userSubject)
+            {
+
+            }
+        }
+    }
+}
